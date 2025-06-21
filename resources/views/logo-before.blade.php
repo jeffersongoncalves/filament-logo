3f8ff4725c4345f8e9b146b3d415098c95662fa6
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-21 10:02:56 -0300

    feat(logo): update responsive styles for logo components
    Improve responsiveness by replacing class-based styling with inline CSS and media queries in `logo-before.blade.php` and `logo-start.blade.php`. Adjustments ensure proper logo display across devices.

--- resources/views/logo-before.blade.php
+++ resources/views/logo-before.blade.php
@@ -1,4 +1,4 @@
-<div class="mx-auto p-2 block lg:hidden">
+<div style="display: block;padding: 0.5rem; @media (min-width: 1024px) {display: none;}">
     @if ($homeUrl = filament()->getHomeUrl())
         <a {{ \Filament\Support\generate_href_html($homeUrl) }}>
             <x-filament-panels::logo />
--- resources/views/logo-start.blade.php
+++ resources/views/logo-start.blade.php
@@ -1,9 +1,10 @@
-<div x-cloak x-data="{}" x-show="!$store.sidebar.isOpen" x-transition.opacity.300ms class="hidden lg:block">
+<div x-cloak x-data="{}" x-show="!$store.sidebar.isOpen" x-transition.opacity.300ms
+     style="display: none; @media (min-width: 1024px) { display: block; }">
     @if ($homeUrl = filament()->getHomeUrl())
         <a {{ \Filament\Support\generate_href_html($homeUrl) }}>
-            <x-filament-panels::logo />
+            <x-filament-panels::logo/>
         </a>
     @else
-        <x-filament-panels::logo />
+        <x-filament-panels::logo/>
     @endif
 </div>
