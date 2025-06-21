67530d7ec5435b3f7183a48db2efa610f83f4e64
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-21 10:22:24 -0300

    feat(filament-logo): add custom styles for logo rendering hooks
    Introduce `styles.blade.php` for logo-related styles and update `LogoServiceProvider` to include a new `HEAD_END` render hook for injecting custom CSS. Improves logo appearance across responsive breakpoints.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,10 +2,6 @@
 
 All notable changes to this project will be documented in this file.
 
-## 2.0.2 - 2025-06-21
-
-**Full Changelog**: https://github.com/jeffersongoncalves/filament-logo/compare/1.0.0...2.0.2
-
 ## 1.0.0 - 2025-06-21
 
 **Full Changelog**: https://github.com/jeffersongoncalves/filament-logo/commits/1.0.0
---
+++ resources/views/styles.blade.php
@@ -0,0 +1,19 @@
+<style>
+    .logo-before {
+        display: flex;
+        padding: 0.5rem;
+        justify-content: center;
+        align-items: center;
+    }
+    .logo-start{
+        display: none;
+    }
+    @media (min-width: 1024px) {
+        .logo-before {
+            display: none;
+        }
+        .logo-start {
+            display: block;
+        }
+    }
+</style>
