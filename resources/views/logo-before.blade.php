51a5567328a05b00dc2a2796d1c80ea82f80563f
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-21 09:45:13 -0300

    feat(dependencies): upgrade filament/filament to v4.0.0-beta6
    Update Filament dependency to the latest beta version (v4.0.0-beta6) for compatibility and access to new features. Minor view adjustments included for logo components.

--- resources/views/logo-before.blade.php
+++ resources/views/logo-before.blade.php
@@ -1,9 +1,9 @@
 <div class="mx-auto p-2 block lg:hidden">
     @if ($homeUrl = filament()->getHomeUrl())
-        <a {{ Filament\Support\generate_href_html($homeUrl) }}>
-            <x-filament-panels::logo/>
+        <a {{ \Filament\Support\generate_href_html($homeUrl) }}>
+            <x-filament-panels::logo />
         </a>
     @else
-        <x-filament-panels::logo/>
+        <x-filament-panels::logo />
     @endif
 </div>
