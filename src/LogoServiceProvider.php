cd46330319b9752ec33f1f9f026b0f1a0fe4415e
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-21 20:42:54 -0300

    refactor(logoserviceprovider): add return type hints to render hooks
    Add `View` return type hints to closure functions registered in `LogoServiceProvider` to enhance code clarity and consistency. Update PHPStan configuration to ignore specific type-related errors.

--- src/LogoServiceProvider.php
+++ src/LogoServiceProvider.php
@@ -4,6 +4,7 @@ namespace JeffersonGoncalves\Filament\Logo;
 
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
+use Illuminate\Contracts\View\View;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -18,7 +19,7 @@ class LogoServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
-        FilamentView::registerRenderHook(PanelsRenderHook::HEAD_END, fn () => view('filament-logo::styles'));
-        FilamentView::registerRenderHook(PanelsRenderHook::TOPBAR_BEFORE, fn () => view('filament-logo::logo-before'));
+        FilamentView::registerRenderHook(PanelsRenderHook::HEAD_END, fn (): View => view('filament-logo::styles'));
+        FilamentView::registerRenderHook(PanelsRenderHook::TOPBAR_BEFORE, fn (): View => view('filament-logo::logo-before'));
     }
 }
